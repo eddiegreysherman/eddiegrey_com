f7ab2674cb8c745fa7fae1d904807497b5a90f8e
eddie.sherman@gmail.com <Gr3750n123>
2017-08-18 06:51:57 -0400

    added some json methods.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
+import uuid
+
 class Post(object):
-    def __init__(self, title, content, author_id, created, published, modified):
+    def __init__(self, title, content, author_id, created, published, modified, _id=None):
+        self._id = uuid.uuid4().hex if _id is None else _id
         self.title = title
         self.content = content
         self.author_id = author_id
@@ -10,9 +13,24 @@ class Post(object):
     def __repr__(self):
         return "<Post {}>".format(self.title)
 
+    def get_posts(self):
+        pass
+
+    def json(self):
+        return {
+            "_id": self._id,
+            "title": self.title,
+            "content": self.content,
+            "author_id": self.author_id,
+            "created": self.created,
+            "published": self.published,
+            "modified": self.modified
+        }
+
 
 class User(object):
-    def __init__(self, username, password, email, fullname):
+    def __init__(self, username, password, email, fullname, _id=None):
+        self._id = uuid.uuid4().hex if _id is None else _id
         self.username = username
         self.password = password
         self.email = email
@@ -21,3 +39,11 @@ class User(object):
     def __repr__(self):
         return "<User {}>".format(self.username)
 
+    def json(self):
+        return {
+            "_id": self._id,
+            "username": self.username,
+            "password": self.password,
+            "email": self.email,
+            "fullname": self.fullname
+        }
--- blog/views/blog.py
+++ blog/views/blog.py
@@ -1,7 +1,7 @@
+
 from flask import Blueprint, render_template
 
 blog = Blueprint('blog', __name__)
-
 
 
 @blog.route('/')
